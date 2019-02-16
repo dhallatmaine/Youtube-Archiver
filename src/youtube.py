9410410c4fd9ae480877112790491e86217865de
Derrick Hall <derrick.hall@nyshex.com>
2019-02-16 00:44:17 -0500

    Get pretty far with progress bars

--- src/youtube.py
+++ src/youtube.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
 import urllib2
-from flask import Flask, flash, request, abort, redirect, Response, url_for, render_template
+import re
+from flask import Flask, request, abort, redirect, Response, url_for, render_template, jsonify
 from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user
 from bs4 import BeautifulSoup
 from celery import Celery
@@ -83,30 +84,93 @@ def login():
 @app.route('/download', methods=['POST'])
 @login_required
 def download():
-    youtube_link = request.form['youtube']
-    audio_only = request.form.get('audio')
+    json = request.get_json()
+    youtube_link = json['youtube_link']
+    audio_only = json['audio']
 
     soup = BeautifulSoup(urllib2.urlopen(youtube_link), features="html.parser")
     title = soup.title.string
 
     if audio_only:
-        subprocess.Popen(['sudo', 'youtube-dl', '-f', 'bestaudio[ext=m4a]', youtube_link])
-        flash('Downloading audio for ' + title)
+        cmd = ['sudo', 'youtube-dl', '-f', 'bestaudio[ext=m4a]', youtube_link]
     else:
-        flash('Downloading video for ' + title)
-        task = video_download.apply_async(args=[youtube_link])
-        print(task.id)
-        #subprocess.Popen(['sudo', 'youtube-dl', youtube_link])
+        cmd = ['sudo', 'youtube-dl', '-f', 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4', youtube_link]
 
-    return redirect(url_for('index'))
+    task = video_download.apply_async(args=[cmd, title, audio_only])
+    return jsonify({}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
 
 @celery.task(bind=True)
-def video_download(self, youtube_link):
-    print('running task')
-    #proc = subprocess.Popen(['sudo', 'youtube-dl', youtube_link], stdout=PIPE)
-    #for line in iter(proc.stdout.readline, b''):
-        #print line,
-    #proc.communicate()
+def video_download(self, cmd, title, audio_only):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    current = 0
+    stage = 'video'
+    percent_ratio = 2.5
+    audio_start = 40
+    if (audio_only):
+        percent_ratio = 1.25
+        audio_start = 0
+
+    for line in iter(proc.stdout.readline, ''):
+        l = line.strip()
+        if l.startswith('[download] Destination') and l.endswith('.mp4'):
+            current = 0
+            message = 'Starting download of video'
+            self.update_state(state='PROGRESS', meta={'current': 0, 'status': message, 'title': title})
+        elif l.startswith('[download] Destination') and l.endswith('.m4a'):
+            current = 40
+            stage = 'audio'
+            message = 'Starting download of audio'
+            self.update_state(state='PROGRESS', meta={'current': 40, 'status': message, 'title': title})
+        elif l.startswith('[ffmpeg] Merging'):
+            current = 85
+            message = 'Merging video and audio'
+            self.update_state(state='PROGRESS', meta={'current': 85, 'status': message, 'title': title})
+        elif l.startswith('Deleting original file') and l.endswith('.mp4 (pass -k to keep)'):
+            current = 90
+            message = 'Clean up original video file'
+            self.update_state(state='PROGRESS', meta={'current': 90, 'status': message, 'title': title})
+        elif l.startswith('Deleting original file') and l.endswith('.m4a (pass -k to keep)'):
+            current = 95
+            message = 'Clean up original audio file'
+            self.update_state(state='PROGRESS', meta={'current': 95, 'status': message, 'title': title})
+        else:
+            percent = re.findall('\s(100|(\d{1,2}(\.\d+)*))%', l)
+            if len(percent) > 0 and len(percent[0]) > 0:
+                per = float(percent[0][0]) / percent_ratio
+                if (stage == 'audio'):
+                    per = (float(percent[0][0]) / percent_ratio) + audio_start
+                current = current + (per - current)
+                message = 'Downloading ' + stage
+                self.update_state(state='PROGRESS', meta={'current': current, 'status': message, 'title': title})
+    proc.communicate()
+    return {'current': 100, 'status': 'Download complete', 'title': title}
+
+@app.route('/status/<task_id>')
+@login_required
+def taskstatus(task_id):
+    task = video_download.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+            'current': 0,
+            'status': 'Pending...',
+            'title': 'Fetching title...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'status': task.info.get('status', ''),
+            'title': task.info.get('title')
+        }
+    else:
+        response = {
+            'state': task.state,
+            'current': 100,
+            'status': str(task.info),
+            'title': task.info.get('title')
+        }
+    return jsonify(response)
 
 @app.route("/logout")
 @login_required
@@ -125,4 +189,4 @@ def load_user(userid):
     return users_repository.get_user_by_id(userid)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=8066, debug=False)
